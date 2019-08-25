99f06f9100d01eb2117bb8306148332dab41f09e
siecola <siecola@gmail.com>
2019-08-25 19:29:21 -0300

    DynamoDB table

--- src/main/java/br/com/siecola/aws_project02/AwsProject02Application.java
+++ src/main/java/br/com/siecola/aws_project02/AwsProject02Application.java
@@ -1,13 +1,19 @@
 package br.com.siecola.aws_project02;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class AwsProject02Application {
+	private static final Logger log = LoggerFactory.getLogger(
+			AwsProject02Application.class);
 
 	public static void main(String[] args) {
 		SpringApplication.run(AwsProject02Application.class, args);
+
+		log.info("Version 0.4.1");
 	}
 
 }
